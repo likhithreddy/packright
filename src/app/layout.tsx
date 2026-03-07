e8529093ef5d6583ee614ae1607acf08e7f16eaf
Jaya Sriharshita Koneti <harshitakoneti@gmail.com>
2026-03-07 13:30:37 -0500

    [#28] chore: configured shadcn ui base components and earthy aesthetics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { Geist } from "next/font/google";
+import { Figtree, DM_Serif_Display } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const figtree = Figtree({ subsets: ['latin'], variable: '--font-figtree' });
+const dmSerifDisplay = DM_Serif_Display({ weight: '400', subsets: ['latin'], variable: '--font-dm-serif' });
 
 export const metadata: Metadata = {
     title: 'PackRight',
@@ -16,8 +17,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en" className={cn("font-sans", geist.variable)}>
-            <body className="antialiased">{children}</body>
+        <html lang="en" className={cn("font-sans", figtree.variable, dmSerifDisplay.variable)}>
+            <body className="antialiased font-sans">{children}</body>
         </html>
     );
 }
