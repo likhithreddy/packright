70ce575be8ee18e7535c2ada4ecadb853bd3446c
Likhith Reddy Rechintala <rlikhith@gmail.com>
2026-03-14 20:27:30 -0400

    [#47] feat: commit E2E HTML report in test-report/e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }], ['list']],
+  reporter: [['html', { outputFolder: 'test-report/e2e', open: 'never' }], ['list']],
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   outputDir: 'test-results/',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
